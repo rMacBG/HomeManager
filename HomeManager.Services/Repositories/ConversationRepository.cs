f81a63366aa8251071058681926aab47d3669cba
Vladimir Vasilev <vladyto_777@abv.bg>
2025-04-15 19:18:49 +0300

    added Conversation repos

---
+++ HomeManager.Services/Repositories/ConversationRepository.cs
@@ -0,0 +1,41 @@
+﻿using HomeManager.Data.Data.Context;
+using HomeManager.Data.Data.Models;
+using HomeManager.Services.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeManager.Services.Repositories
+{
+    public class ConversationRepository : IConversationRepository
+    {
+        private readonly HomeManagerDbContext _context;
+        public ConversationRepository(HomeManagerDbContext context)
+        {
+            _context = context;
+        }
+        public async Task AddAsync(Conversation conversation)
+        {
+            await _context.Conversations.AddAsync(conversation);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<Conversation?> GetByIdAsync(Guid id)
+        {
+            return await _context.Conversations
+                .Include(c => c.UsersConversations)
+                .FirstOrDefaultAsync(c => c.Id == id);
+        }
+
+        public async Task<IEnumerable<Conversation>> GetByUserId(Guid userId)
+        {
+            return await _context.Conversations
+                .Where(x => x.UsersConversations.Any(uc => uc.UserId == userId))
+                .Include(x => x.UsersConversations)
+                .ToListAsync();
+        }
+    }
+}
---
+++ HomeManager.Services/Repositories/Interfaces/IConversationRepository.cs
@@ -0,0 +1,18 @@
+﻿using HomeManager.Data.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeManager.Services.Repositories.Interfaces
+{
+    public interface IConversationRepository
+    {
+        Task AddAsync(Conversation conversation);
+
+        Task<Conversation?> GetByIdAsync(Guid id);
+
+        Task<IEnumerable<Conversation>> GetByUserId(Guid userId);
+    }
+}
