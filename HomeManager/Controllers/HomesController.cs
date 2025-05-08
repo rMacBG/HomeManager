750799edfe44134445db68c07fe1cbcf20c80b32
Vladimir Vasilev <vladyto_777@abv.bg>
2025-05-08 18:53:45 +0300

    basic thigns for the homes and stuff

---
+++ HomeManager/Controllers/HomesController.cs
@@ -0,0 +1,32 @@
+﻿using HomeManager.Data.Data.Dtos;
+using HomeManager.Services.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeManager.Controllers
+{
+    public class HomesController : Controller
+    {
+        private readonly IHomeService _homeService;
+
+        public HomesController(IHomeService homeService)
+        {
+            _homeService = homeService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var homes = _homeService.GetAllAsync();
+            if (homes == null)
+                return View(new List<HomeDto>());
+            return View(homes);
+        }
+
+
+        public async Task<IActionResult> Details(Guid id)
+        {
+            var home = _homeService.GetByIdAsync(id);
+            if (home == null) 
+                return NotFound();
+            return View(home);
+        }
+    }
+}
