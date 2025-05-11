d742273133837cce848530ecb7462230bc92dcd4
Vladimir Vasilev <vladyto_777@abv.bg>
2025-05-11 20:26:22 +0300

    Partially Implemented the Message service, needs more work

--- HomeManager.Services/Services/MessageService.cs
+++ HomeManager.Services/Services/MessageService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using HomeManager.Data.Data.Dtos;
+using HomeManager.Services.Repositories.Interfaces;
+using HomeManager.Services.Services.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +9,40 @@ using System.Threading.Tasks;
 
 namespace HomeManager.Services.Services
 {
-    public class MessageService
+    public class MessageService : IMessageService
     {
+        private readonly IMessageRepository _messageRepository;
 
+        public MessageService(IMessageRepository messageRepository)
+        {
+            _messageRepository = messageRepository;  
+        }
+        public async Task<IEnumerable<MessageDto>> GetMessagesAsync(Guid conversationId)
+        {
+            var messages = await _messageRepository.GetByConversationIdAsync(conversationId);
+
+            return messages.Select(x => new MessageDto
+            {
+                Id = x.Id,
+                Content = x.Content,
+                SenderId = x.SenderId,
+                MessageStatus = x.Status,
+                SentAt = x.SentAt,
+            });
+
+        }
+
+        public async Task<Guid> SendMessageAsync(CreateMessageDto dto)
+        {
+            var message = new CreateMessageDto
+            {
+                ConversationId = dto.ConversationId,
+                Content = dto.Content,
+                SenderId = dto.SenderId,
+               
+            };
+            return message.SenderId;
+            
+        }
     }
 }
